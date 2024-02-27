252c77915614c5bb716444ab669e46c0b7694c40
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-28 00:31:38 +0530

    add pre installation

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import postRouter from "./routes/post.routes.js";
 import commentsRouter from "./routes/comments.routes.js";
+import path from "path";
+const __dirname = path.resolve();
 const app = express();
 //
 // predefined middleware
@@ -23,7 +25,11 @@ app.use(
 
 // routes import
 app.use("/api/v1/users", authRouter);
-app.use("/api/v1/post", postRouter)
+app.use("/api/v1/post", postRouter);
 
 app.use("/api/v1/comments", commentsRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 export default app;
