61e19ee4c96f33466c356aae6020e2d833c2261a
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-27 23:55:48 +0530

    updating auth controller

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_URL,
+        target: "http://localhost:3000",
         secure: false,
       },
     },
